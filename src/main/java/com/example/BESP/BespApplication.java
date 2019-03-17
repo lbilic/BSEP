05710093e2513312f40b14e82dc5d32dfb3550cc
Ognjen <ogauzz1@gmail.com>
2019-03-17 13:33:25 +0100

    Front back setup

---
+++ src/main/java/com/example/BESP/BespApplication.java
@@ -0,0 +1,13 @@
+package com.example.BESP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BESP/BespApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.BESP;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BespApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
